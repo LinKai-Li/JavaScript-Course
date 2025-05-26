44b161b08e52a7ea962c12e2116f84e2f053db23
Link Li <link.li@sigmatm.com.au>
2025-05-27 01:13:56 +1000

    Coding Challenge #1

---
+++ Challenge/09-Data-Structures-Operators/Coding Challenge #1/script.js
@@ -0,0 +1,68 @@
+"use strict";
+
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// 1.
+const [players1, players2] = game.players;
+
+// 2.
+const [gk, ...fieldPlayers] = players1;
+
+// 3.
+const allPlayers = [...players1, ...players2];
+
+// 4.
+const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
+
+// 5.
+const { team1, x, team2 } = game.odds;
+
+// 6.
+function printGoals(...playerNames) {
+  console.log(...playerNames);
+  console.log(playerNames.length);
+}
+printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
+printGoals(...game.scored);
+
+// 7.
+console.log(team1 < team2 && "team1");

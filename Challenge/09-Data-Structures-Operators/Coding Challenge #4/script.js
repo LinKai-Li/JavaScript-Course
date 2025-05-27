364d526f176c701ca86075362aec337c4cf5cd0c
Link Li <link.li@sigmatm.com.au>
2025-05-27 15:56:22 +1000

    Coding Challenge #4

---
+++ Challenge/09-Data-Structures-Operators/Coding Challenge #4/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+document.body.append(document.createElement("textarea"));
+document.body.append(document.createElement("button"));
+
+const button = document.querySelector("button");
+button.addEventListener("click", function () {
+  const text = document.querySelector("textarea").value;
+  const textArray = text.split("\n");
+  for (const [index, text] of textArray.entries()) {
+    const lower_text = text.trim().toLowerCase();
+    const [first_part, second_part] = lower_text.split("_");
+    const camelCase = [first_part];
+    camelCase.push(
+      second_part.replace(second_part[0], second_part[0].toUpperCase())
+    );
+    const result = camelCase.join("");
+    console.log(`${result}\t${"✅".repeat(index + 1)}`);
+  }
+});

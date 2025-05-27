1cb7df1069806a18941d55372a7e85b9efc85741
Link Li <link.li@sigmatm.com.au>
2025-05-27 11:58:04 +1000

    Coding Challenge #3

---
+++ Challenge/09-Data-Structures-Operators/Coding Challenge #3/script.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const gameEvents = new Map([
+  [17, "⚽️ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽️ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽️ GOAL"],
+  [80, "⚽️ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
+
+// 1.
+const set = new Set();
+for (const event of gameEvents.values()) {
+  set.add(event);
+}
+console.log([...set]);
+
+// 2.
+gameEvents.delete(64);
+
+// 3.
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+// 4.
+for (const [key, value] of gameEvents) {
+  if (key <= 45) {
+    console.log(`[FIRST HALF] ${key}: ${value}`);
+  } else {
+    console.log(`[SECOND HALF] ${key}: ${value}`);
+  }
+}

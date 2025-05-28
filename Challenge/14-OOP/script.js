f7b252b8a5690af67a193c5741e208ebc25e4286
Link Li <link.li@sigmatm.com.au>
2025-05-28 21:38:06 +1000

    Coding Challenge #1

---
+++ Challenge/14-OOP/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+function Car(make, speed) {
+  this.make = make;
+  this.speed = speed;
+}
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(this.speed);
+};
+
+Car.prototype.break = function () {
+  this.speed -= 5;
+  console.log(this.speed);
+};
+
+const car1 = new Car("BMW", 120);
+const car2 = new Car("Mercedes", 95);
+
+car1.accelerate();
+car1.break();
+
+car2.accelerate();
+car2.break();

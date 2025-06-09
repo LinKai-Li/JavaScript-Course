80f9ff6bde5f736825a876b6a14908489061b6aa
Link Li <link.li@sigmatm.com.au>
2025-06-09 23:02:59 +1000

    Correct Answer

--- Challenge/11-Array/Coding Challenge #5/script.js
+++ Challenge/11-Array/Coding Challenge #5/script.js
@@ -3,35 +3,31 @@
 const dogs = [
   { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
   { weight: 8, curFood: 200, owners: ['Matilda'] },
-  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John', 'Leo'] },
+  { weight: 18, curFood: 244, owners: ['Joe'] },
   { weight: 32, curFood: 340, owners: ['Michael'] },
 ];
 
-const foodWarning = ({ curFood, recommendedFood }) => {
-  if (curFood > recommendedFood * 1.1) {
-    return 'Too Much';
-  }
-  if (curFood < recommendedFood * 0.9) {
-    return 'Too Little';
-  }
-  return 'Normal';
-};
-
 // 1.
-dogs.forEach(dog => (dog['recommendedFood'] = dog.weight ** 0.75 * 28));
+dogs.forEach(
+  dog => (dog['recommendedFood'] = Math.floor(dog.weight ** 0.75 * 28))
+);
 console.log(dogs);
 
 // 2.
 const sarahDog = dogs.find(dog => dog.owners.includes('Sarah'));
-console.log(sarahDog);
-console.log(foodWarning(sarahDog));
+console.log(
+  `Sarah's dog eats too ${
+    sarahDog.curFood > sarahDog.recommendedFood ? 'much' : 'little'
+  }`
+);
 
 // 3.
 const ownersEatTooMuch = dogs
-  .filter(dog => foodWarning(dog) === 'Too Much')
+  .filter(dog => dog.curFood > dog.recommendedFood)
   .flatMap(dog => dog.owners);
 const ownersEatTooLittle = dogs
-  .filter(dog => foodWarning(dog) === 'Too Little')
+  .filter(dog => dog.curFood < dog.recommendedFood)
   .flatMap(dog => dog.owners);
 console.log(ownersEatTooMuch);
 console.log(ownersEatTooLittle);
@@ -44,13 +40,32 @@ console.log(ownersEatTooLittle.join(' and ') + "'s dogs eat too little!");
 console.log(dogs.some(dog => dog.curFood === dog.recommendedFood));
 
 // 6.
-console.log(dogs.some(dog => foodWarning(dog) === 'Normal'));
+const checkEatingOkay = dog =>
+  dog.curFood < dog.recommendedFood * 1.1 &&
+  dog.curFood > dog.recommendedFood * 0.9;
+
+console.log(dogs.every(checkEatingOkay));
 
 // 7.
-const okayDogs = dogs.filter(dog => foodWarning(dog) === 'Normal');
+const okayDogs = dogs.filter(checkEatingOkay);
 console.log(okayDogs);
 
 // 8.
+const groupedDogsByPortion = Object.groupBy(dogs, dog => {
+  if (dog.curFood > dog.recommendedFood) return 'too-much';
+  if (dog.curFood < dog.recommendedFood) return 'too-little';
+  if (dog.curFood === dog.recommendedFood) return 'exact';
+});
+console.log(groupedDogsByPortion);
+
+// 9.
+const groupedDogsByOwner = Object.groupBy(
+  dogs,
+  dog => `${dog.owners.length}-owners`
+);
+console.log(groupedDogsByOwner);
+
+// 10.
 const sortedDogs = dogs
   .slice()
   .sort((a, b) => a.recommendedFood - b.recommendedFood);
